fdec7b24b4b0c903dd45d0a6fd373a4fff203b54
ahmedhalac <ahmedhalac@outlook.com>
2025-03-05 23:36:31 +0100

    Adde hero section content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${dmSans.variable}`}>
-        {children}
-      </body>
+      <body className={`${inter.variable} ${dmSans.variable}`}>{children}</body>
     </html>
   );
 }
--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -65,6 +65,34 @@ export default function HeroSection() {
           </div>
         )}
       </nav>
+
+      <section className="relative h-[829px] bg-primary text-white font-inter">
+        <Image
+          src="/waves.svg"
+          alt="Waves"
+          fill
+          quality={100}
+          sizes="100vw"
+        ></Image>
+        <div className="absolute inset-0 flex items-center flex-row justify-center text-center z-10 gap-10 max-w-[1280px] mx-auto">
+          <div className="flex flex-col gap-8 w-1/2">
+            <h1 className="text-6xl">Get More Done with whitespace</h1>
+            <p>
+              Project management software that enables your teams to
+              collaborate, plan, analyze and manage everyday tasks
+            </p>
+          </div>
+          <div className="w-1/2">
+            <Image
+              src="/hero.jpg"
+              alt="Hero section image"
+              quality={100}
+              width={200}
+              height={200}
+            ></Image>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
